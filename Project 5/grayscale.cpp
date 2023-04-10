d216b6d2f87cb11269c15e7a01a239dd00b7cfaa
andreshibata <andremassanobush@gmail.com>
2023-04-09 22:28:59 -0400

    Update grayscale.cpp
    Implement the native grayscale

--- Project 5/grayscale.cpp
+++ Project 5/grayscale.cpp
@@ -6,6 +6,24 @@
 // Function: Grayscale (naive implementation)
 void grayscale(FrameData* f_dat) {
     // Do stuff to f_dat->frame
+
+    cv::Mat& src = f_dat->frame;
+    for(int i=0;i<src.rows;i++){
+        for(int j=0;j<src.cols;j++){
+
+            Vec3b & color = src.at<Vec3b>(i,j);
+
+            int sum = color[0]+color[1]+color[2];
+            int sum /= 3;
+            color[0] = sum;
+            color[1] = sum;
+            color[2] = sum;
+            
+            src.at<Vec3b>(Point(i,j)) = color;
+        }
+    }
+
+
 }
 
 // Function: Grayscale (SIMD implementation)
